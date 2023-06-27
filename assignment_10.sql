f2b82a8c585d520e4402aed7111069b8425a9aa2
naa-7 <nalawad000@citymail.cuny.edu>
2023-06-27 09:58:10 -0400

    finished app

---
+++ assignment_10.sql
@@ -0,0 +1,63 @@
+# Creating the database
+	create database employees;
+	
+# Creating tables:
+
+## creating employees table:	
+	create table employees (
+		employee_id serial primary key,
+		first_name varchar(50),
+		last_name varchar(50),
+		email varchar(100));
+
+## creating departments table:	
+	create table departments (
+		department_id serial primary key,
+		department_name varchar(50),
+		location varchar(50));
+
+## creating salaries table:	
+	create table salaries (
+		salary_id serial primary key,
+		employee_id int,
+		salary_amount numeric(10,2),
+		start_date date,
+		foreign key (employee_id) references employees(employee_id));
+
+## creating positions table: 
+	create positions (
+		position_id serial primary key,
+		employee_id int,
+		title varchar(50),
+		start_date date,
+		foreign key (employee_id) references employees(employee_id));
+		
+# Inserting values into tables:
+
+## Inserting values into employees table:
+	insert into employees (first_name, last_name, email) VALUES
+		  ('John', 'Doe', 'johndoe@example.com'),
+		  ('Jane', 'Smith', 'janesmith@example.com'),
+		  ('Michael', 'Johnson', 'michaeljohnson@example.com'),
+		  ('Emily', 'Davis', 'emilydavis@example.com');
+		  
+## Inserting values into departments table:
+	insert into departments (department_name, location) VALUES
+		  ('IT', 'New York'),
+		  ('HR', 'Los Angeles'),
+		  ('Marketing', 'Chicago'),
+		  ('Finance', 'San Francisco');
+
+## Inserting values into salaries table:
+	insert into salaries (employee_id, salary_amount, start_date) VALUES
+		  (1, 5000, '2021-01-01'),
+		  (2, 6000, '2021-02-15'),
+		  (3, 5500, '2021-03-10'),
+		  (4, 7000, '2021-04-20');
+		  
+## Inserting values into positions table:
+	insert into positions (employee_id, title, start_date) VALUES
+		  (1, 'Software Engineer', '2021-01-01'),
+		  (2, 'HR Manager', '2021-02-15'),
+		  (3, 'Marketing Analyst', '2021-03-10'),
+		  (4, 'Financial Analyst', '2021-04-20');
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'employees',
+  password: '123',
+  port: 5432,
+});
+
+app.use(express.json());
+
+app.post('/data', async (req, res) => {
+  try {
+    const {first_name, last_name, email} = req.body;
+    console.log('Request Body:', req.body);
+    const query = 'INSERT INTO employees (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING *';
+    const result = await pool.query(query, [first_name, last_name, email]);
+    console.log(result);
+
+    res.status(200).json({ message: 'Data inserted successfully', data: {employees: result.rows[0]} });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.get('/data', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM employees';
+    const results = await pool.query(query);
+
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
